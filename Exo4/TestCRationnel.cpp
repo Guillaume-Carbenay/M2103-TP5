ecf44f26847f3409c644aff35f2f65c299d2235f
Guillaume Carbenay <37800990+Guillaume-Carbenay@users.noreply.github.com>
2018-04-17 17:24:07 +0200

    Exercice 4

---
+++ Exo4/TestCRationnel.cpp
@@ -0,0 +1,68 @@
+/**
+ *
+ * \file   : SqueletteMain.cpp
+ *
+ * \author :
+ *
+ * \date   :
+ *
+**/
+
+#include <iostream>
+#include <exception>
+#include "Rationnel.h"
+#include "CstCodErr.h"
+#include "CException.h"
+using namespace std;
+using namespace nsUtil;
+
+namespace
+{
+    int divisionEntiere (const int & e1, const int & e2) throw (CException)
+    {
+        if (e2 == 0) throw nsUtil::CException("Erreur Division par zero", nsUtil::KExcDivZero);
+        return e1/e2;
+    }
+
+    void testDivisionParZero (void)
+    {
+        int num1, denom1, num2, denom2;
+        cin >> num1;
+        cin >> denom1;
+        nsMath::Rationnel dividende(num1, denom1);
+        cin >> num2;
+        cin >> denom2;
+        nsMath::Rationnel diviseur(num2, denom2);
+        nsMath::Rationnel result = dividende / diviseur;
+        result.display();
+        cout << endl;
+    } // testDivisionParZero ()
+} // namespace
+
+int main (void)
+{
+    while(true)
+    {
+        try
+        {
+            testDivisionParZero ();
+        }
+        catch (const CException & e)
+        {
+            cerr << "Erreur        : " << e.getLibelle () << '\n'
+                 << "Code d'erreur = " << e.getCodErr ()  << '\n';
+            return e.getCodErr();
+        }
+        catch (const exception & e)
+        {
+            cerr << "Exception standard : " << e.what () << '\n';
+            return KExcStd;
+        }
+        catch (...)
+        {
+            cerr << "Exception inconnue\n";
+            return KExcInconnue;
+        }
+    }
+    return KNoExc;
+} // main()
